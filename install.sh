90d59d1e29a6f5cf636ca5cc3fd80c29040465b8
foolon <43252350+f00l0n@users.noreply.github.com>
2018-09-13 18:47:46 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 #install i3wm, qutebrowser, skype and dependencies
 sudo dnf install -y qutebrowser i3 vim zsh ranger w3m feh ImageMagick cava libxcb-devel xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel xcb-util-xrm-devel yajl-devel libXrandr-devel startup-notification-devel libev-devel xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel libxkbcommon-x11-devel pcre-devel pango-devel git gcc automake i3status i3lock
-dnf -y install https://repo.skype.com/latest/skypeforlinux-64-alpha.rpm
+sudo dnf -y install https://repo.skype.com/latest/skypeforlinux-64-alpha.rpm
+
+#installing compton and rofi
+sudo dnf copr enable yaroslav/i3desktop
+sudo dnf install -y compton rofi
 
 #install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
